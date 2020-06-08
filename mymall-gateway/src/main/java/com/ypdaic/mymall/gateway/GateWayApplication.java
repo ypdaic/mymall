137e64bd0c42af76184637dcbc3188a72bc14947
ypdaic <841943896@qq.com>
2020-06-08 18:29:44 +0800

    代码提交。

---
+++ mymall-gateway/src/main/java/com/ypdaic/mymall/gateway/GateWayApplication.java
@@ -0,0 +1,20 @@
+package com.ypdaic.mymall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GateWayApplication
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2020-06-08
+ * @Version 0.1
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+
+}
