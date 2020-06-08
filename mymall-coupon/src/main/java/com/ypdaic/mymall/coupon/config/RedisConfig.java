53b221edea6dd3b4235989aa65cb69e76e3ff5f4
ypdaic <841943896@qq.com>
2020-06-08 10:26:45 +0800

    代码生成

---
+++ mymall-coupon/src/main/java/com/ypdaic/mymall/coupon/config/RedisConfig.java
@@ -0,0 +1,150 @@
+package com.ypdaic.mymall.coupon.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ypdaic.mymall.common.cache.RedisCacheManagerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scripting.ScriptSource;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(CacheProperties.class)
+public class RedisConfig {
+
+    /**
+     * 配置自定义redisTemplate
+     *
+     * @return
+     */
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setValueSerializer(valueSerializer());
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(keySerializer());
+        template.setHashKeySerializer(keySerializer());
+        template.setHashValueSerializer(valueSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Autowired
+    private CacheProperties cacheProperties;
+
+    @Autowired
+    RedisCacheManagerInterceptor redisCacheManagerInterceptor;
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(cacheProperties.getRedis().getTimeToLive())
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()));
+
+        RedisCacheConfiguration config2 = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5000))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()));
+
+        RedisCacheConfiguration config3 = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(600000))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()));
+
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.builder(redisConnectionFactory);
+        builder = builder.cacheDefaults(config);
+        //获取初始化缓存库名称
+        List<String> cacheNames = this.cacheProperties.getCacheNames();
+        if (!cacheNames.isEmpty()) {
+            builder.initialCacheNames(new LinkedHashSet<>(cacheNames));
+        }
+
+        // 对每个缓存空间应用不同的配置
+        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("APP_CACHE", config);
+        configMap.put("WX_CACHE", config2);
+        configMap.put("WX_QRCODE_CACHE", config3);
+
+        builder.withInitialCacheConfigurations(configMap);
+
+        RedisCacheManager redisCacheManager = builder
+                .transactionAware()
+                .build();
+
+
+        ProxyFactory factory = new ProxyFactory();
+        factory.setExposeProxy(true);
+        redisCacheManagerInterceptor.setRedisCacheManager(redisCacheManager);
+        factory.addAdvisor(new DefaultPointcutAdvisor(redisCacheManagerInterceptor));
+        factory.setTarget(redisCacheManager);
+        redisCacheManager = (RedisCacheManager) factory.getProxy(RedisCacheManager.class.getClassLoader());
+
+
+        return redisCacheManager;
+    }
+
+    @Bean
+    public RedisSerializer keySerializer() {
+        return new StringRedisSerializer();
+    }
+
+    @Bean
+    public RedisSerializer valueSerializer() {
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        return serializer;
+    }
+
+    /**
+     * lua脚本
+     * 在应用程序上下文中配置DefaultRedisScript的单个实例是理想的，以避免在每次执行脚本时重新计算脚本的SHA1。
+     *
+     * @return
+     */
+//    @Bean
+    public RedisScript<Boolean> retryCountCheckScript() {
+
+        ScriptSource scriptSource = new ResourceScriptSource(new ClassPathResource("lua/retryCountCheck.lua"));
+        try {
+            return RedisScript.of(scriptSource.getScriptAsString(), Boolean.class);
+        } catch (IOException e) {
+            log.error("lua 脚本加载失败", e);
+        }
+        return RedisScript.of("", Boolean.class);
+    }
+
+
+}
---
+++ mymall-member/src/main/java/com/ypdaic/mymall/member/config/MybatisPlusConfig.java
@@ -0,0 +1,76 @@
+package com.ypdaic.mymall.member.config;
+
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.plugin.Interceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
+
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NOT_SUPPORTED;
+
+@Configuration
+@MapperScan("com.ypdaic.mymall.member.**.mapper")
+public class MybatisPlusConfig {
+
+    @Autowired
+    private MybatisPlusProperties mybatisPlusProperties;
+
+    @Bean
+    public MybatisSqlSessionFactoryBean getSqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setMapperLocations(mybatisPlusProperties.resolveMapperLocations());
+        factory.setPlugins(new Interceptor[]{paginationInterceptor(), performanceInterceptor()});
+        GlobalConfig globalConfig = mybatisPlusProperties.getGlobalConfig();
+        factory.setGlobalConfig(globalConfig);
+        return factory;
+    }
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
+        return paginationInterceptor;
+    }
+
+    /**
+     * 开启spring事物管理
+     * @return
+     */
+    @Bean
+    public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate(DataSource dataSource) {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(getDataSourceTransactionManager(dataSource));
+        transactionTemplate.setPropagationBehavior(PROPAGATION_NOT_SUPPORTED);
+        return transactionTemplate;
+    }
+
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setFormat(true);
+        // 将sql写入log文件，不只是在控制台打印，还需要结合druid的merge-sql功能，才能打印完整的sql
+        performanceInterceptor.setWriteInLog(true);
+        return performanceInterceptor;
+    }
+}
