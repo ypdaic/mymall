460239ce1eb5dad97af3294d740fc78603eeab37
ypdaic <841943896@qq.com>
2020-06-08 10:52:52 +0800

    代码生成

--- mymall-coupon/src/main/java/com/ypdaic/mymall/coupon/CouponApplication.java
+++ mymall-coupon/src/main/java/com/ypdaic/mymall/coupon/CouponApplication.java
@@ -2,7 +2,11 @@ package com.ypdaic.mymall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@ComponentScan(value = {"com.ypdaic.mymall.coupon.**", "com.ypdaic.mymall.common.**"})
 @SpringBootApplication
 public class CouponApplication {
 
--- mymall-order/src/main/java/com/ypdaic/mymall/order/OrderApplication.java
+++ mymall-order/src/main/java/com/ypdaic/mymall/order/OrderApplication.java
@@ -2,7 +2,11 @@ package com.ypdaic.mymall.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@ComponentScan(value = {"com.ypdaic.mymall.order.**", "com.ypdaic.mymall.common.**"})
 @SpringBootApplication
 public class OrderApplication {
 
