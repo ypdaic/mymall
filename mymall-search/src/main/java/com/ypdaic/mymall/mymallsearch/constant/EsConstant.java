85e211a17b2c9dc8bbf35e6444be9edb2159ce84
ypdaic <841943896@qq.com>
2020-06-15 18:09:30 +0800

    代码提交。

--- mymall-search/src/main/java/com/ypdaic/mymall/mymallsearch/constant/EsConstant.java
+++ mymall-search/src/main/java/com/ypdaic/mymall/mymallsearch/constant/EsConstant.java
@@ -2,6 +2,6 @@ package com.ypdaic.mymall.mymallsearch.constant;
 
 public class EsConstant {
     //SKU数据在ES中的索引
-    public static final String PRODUCT_INDEX="gulimall_product";
+    public static final String PRODUCT_INDEX="product";
     public static final Integer PRODUCT_PAGESIZE = 4;
 }
